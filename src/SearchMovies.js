9c842e1c107c178b05da969e176e60c0ab0afd8f
ptonline40 <ptonline40@gmail.com>
2023-11-06 14:11:42 -0500

    feat: set up app and build movie search functionality

---
+++ src/SearchMovies.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function SearchMovies() {
+  const [movieTitle, setMovieTitle] = useState("");
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    const url = `https://www.omdbapi.com/?apikey=d52129d9&t=${movieTitle}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form onSubmit={searchMovie}>
+      <label htmlFor="query">Movie Title: </label>
+      <input
+        type="text"
+        name="query"
+        value={movieTitle}
+        onChange={(e) => setMovieTitle(e.target.value)}
+        placeholder="Enter a movie title"
+        role="search"
+      />
+      <button type="submit">Search Movie</button>
+    </form>
+  );
+}
+
+export default SearchMovies;
